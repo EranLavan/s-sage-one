727190394a2830b4966275a54ce7183fe6503b4e
Grigorii Belov <belovgrigory139@gmail.com>
2023-09-12 22:20:52 +0300

    modified:   src/components/Milim.css 	modified:   src/components/Milim.js

--- src/components/Milim.js
+++ src/components/Milim.js
@@ -2,12 +2,12 @@ import React, { useState } from 'react';
 import './Milim.css';
 
 function Milim() {
-  const inputForm = document.querySelector('input');
-  const [inputContent, setInputContent] = useState(document.querySelector('input')?.value);
+  const [inputValue, setInputValue] = useState('');
   const [language, setLanguage] = useState('english');
   const [score, setScore] = useState(0);
   const [word, setWord] = useState(0);
-  const [message, setMessage] = useState('To start please first press the Start button');
+  const [message, setMessage] = useState('');
+  const [showPronunciation, setShowPronunciation] = useState(false);
   const [showFinalResults, setFinalResults] = useState(false);
   
   const changeLanguage = () => {
@@ -21,152 +21,177 @@ function Milim() {
       id: 1,
       english: 'Hello',
       russian: 'Здравствуйте',
-      hebrew: 'שלום'
+      hebrew: 'שלום',
+      pronuncEng: 'shalom'
     },
 
     {
       id: 2,
       english: 'Good morning',
       russian: 'Доброе утро',
-      hebrew: 'בוקר טוב'
+      hebrew: 'בוקר טוב',
+      pronuncEng: 'boker tov'
     },
 
     {
       id: 3,
       english: 'City',
       russian: 'Город',
-      hebrew: 'עיר'
+      hebrew: 'עיר',
+      pronuncEng: 'ir'
     },
 
     {
       id: 4,
       english: 'Street',
       russian: 'Улица',
-      hebrew: 'רחוב'
+      hebrew: 'רחוב',
+      pronuncEng: 'rekhOv'
     },
 
     {
       id: 5,
       english: 'Sun',
       russian: 'Солнце',
-      hebrew: 'שמש'
+      hebrew: 'שמש',
+      pronuncEng: 'shEmesh'
     },
 
     {
       id: 6,
       english: 'Tree',
       russian: 'Дерево',
-      hebrew: 'עץ'
+      hebrew: 'עץ',
+      pronuncEng: 'ets'
     },
 
     {
       id: 7,
       english: 'Air',
       russian: 'Воздух',
-      hebrew: 'אוויר'
+      hebrew: 'אוויר',
+      pronuncEng: 'avir'
     },
 
     {
       id: 8,
       english: 'Red',
       russian: 'Красный',
-      hebrew: 'אדום'
+      hebrew: 'אדום',
+      pronuncEng: 'adOm'
     },
 
     {
       id: 9,
       english: 'Sea',
       russian: 'Море',
-      hebrew: 'ים'
+      hebrew: 'ים',
+      pronuncEng: 'yam'
     },
 
     {
       id: 10,
       english: 'Haifa',
       russian: 'Хайфа',
-      hebrew: 'חיפה'
+      hebrew: 'חיפה',
+      pronuncEng: 'KheifA'
     },
 
     {
       id: 11,
       english: 'To speak (infinitive)',
       russian: 'Разговаривать (инфинитив)',
-      hebrew: 'לדבר'
+      hebrew: 'לדבר',
+      pronuncEng: 'ledabEr'
     },
 
     {
       id: 12,
       english: 'see (present, single, m)',
       russian: 'Видеть (настоящее время, ед. число, муж. род)',
-      hebrew: 'רואה'
+      hebrew: 'רואה',
+      pronuncEng: 'roE'
     }
   ]
 
   const checkAnswerEasy = () => {
-    // console.log(inputForm?.value);
+      if (inputValue === '') {
+        setMessage(`Type the word inside the input please`)
+      
+      } else if (inputValue === words[word].hebrew && word < words.length - 1) {
 
-    if (inputForm?.value === words[word].hebrew) {
+        console.log(inputValue)
         setScore(score + 1);
-        setMessage(`Correct! The answer is <span id='orange'>${words[word].hebrew}</span>. <br /> Click 'Next' button to try next word.`)
-    } else {
-        setMessage(`That's incorrect. Try again or press 'Next' button to skip to the next word`)
-    }
+        setMessage(`Correct! '${words[word].english}' is <span id='orange'>${words[word].hebrew}</span>.
+         <span id='pronunciation'}>Show pronunciation</span>`);
+        setWord(word + 1)
+
+      } else if (word < words.length - 1) {
+
+        setMessage(`${inputValue} was incorrect. Try next word.`);
+        setWord(word + 1)
+      } else {
+        setFinalResults(true);
+      }
+    setInputValue('');
   }
 
-  const checkAnswerHard = () => {
-    if (inputForm?.value === words[word].hebrew && word < words.length - 1) {
-      setScore(score + 1);
-      setWord(word + 1);
+  // const checkAnswerHard = () => {
+  //   if (inputValue?.value === words[word].hebrew && word < words.length - 1) {
+  //     setScore(score + 1);
+  //     setWord(word + 1);
 
-    }
-  }
+  //   }
+  // }
 
-  const next = () => {
-    if (word < (words.length - 1)) {
-      setWord(word + 1)
-    } else {
-      setFinalResults(true);
-    }
-  }
+  // const next = () => {
+  //   if (word < (words.length - 1)) {
+  //     setWord(word + 1)
+  //   } else {
+  //     setFinalResults(true);
+  //   }
+  // }
 
-  const activate = () => {
+  const restart = () => {
     setFinalResults(false);
     setScore(0);
     setWord(0);
-    setMessage(`Now you can start!`)
+    setMessage(``)
   }
 
-  const easy = () => {
-    // IS THIS NECESSARY??
+  const pronounce = () => {
+      setShowPronunciation(true)
   }
 
-  const hard = () => {
-    // IS THIS NECESSARY?
-  }
+  // const easy = () => {
+  //   // IS THIS NECESSARY??
+  // }
 
-  //document.addEventListener('DOMContentLoaded', function consoleLog() {console.log(inputForm?.value)})
+  // const hard = () => {
+  //   // IS THIS NECESSARY?
+  // }
 
 
 
   return (
   <>
-    <select className='select-difficulty'>
+    {/* <select className='select-difficulty'>
       <option 
         title='You can guess the word unlimited amount of times' 
         onChange={() => easy()}>
       Easy
       </option>
 
-      {/* Recommended to use value or defaultValue props 
+      Recommended to use value or defaultValue props 
       (already asked about this, look there in the conversation)
-      for the option that should be selected as a basic option */}
+      for the option that should be selected as a basic option
 
       <option 
         title='If you give an incorrect answer, the game goes on to the next word'
         onChange={() => hard()}>
           Hard
       </option>
-    </select>
+    </select> */}
   
 
 
@@ -180,7 +205,7 @@ function Milim() {
         </div>
 
       <div className='center'>
-        <button className='click-button' onClick={() => activate()}>
+        <button className='click-button' onClick={() => restart()}>
           Try again!
         </button>
       </div>
@@ -195,25 +220,34 @@ function Milim() {
           {words[word].english}
         </h1>
 
-        <div><input className='input'></input></div>
+        <div>
+          <input 
+            className='input'
+            type='text'
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}>
+          </input>
+        </div>
 
         <div className='button-div'>
-          <button className='click-button' onClick={() => activate()}>
-            Start
-          </button>
           <button className='click-button' onClick={() => checkAnswerEasy()}>
             Check!
           </button>
-          <button className='click-button' onClick={() => next()}>
+          {/* <button className='click-button' onClick={() => next()}>
             Next
-          </button>
+          </button> */}
         </div>
 
         <h3 className='h3'>Current score: {score}</h3>
-        <div className='h3' id='message'>
-          {message.split('<br />').map((line, index) => (
-            <div key={index} dangerouslySetInnerHTML={{ __html: line }} />
-          ))}
+        <div className='one-line-div'>
+          <div className='h3' id='message' onClick={() => pronounce()}>
+            {message.split('<br />').map((line, index) => (
+              <div key={index} dangerouslySetInnerHTML={{ __html: line }} />
+            ))}
+          </div>
+          <div id='hidden'>
+            Hidden Div
+          </div>
         </div>
 
 
