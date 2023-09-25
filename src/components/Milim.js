69d98424f4de682977f8548837fb555e45d9a485
Grigorii Belov <belovgrigory139@gmail.com>
2023-09-25 23:40:16 +0300

    modified:   src/components/Milim.js

--- src/components/Milim.js
+++ src/components/Milim.js
@@ -2,136 +2,145 @@ import React, { useState, useRef, useEffect } from 'react';
 // import { useLanguage } from './LanguageContext';
 import './Milim.css';
 
-const alreadyUsedWords = [];
+let alreadyUsedWords = [];
+
+const words = [
+  {
+    id: 1,
+    english: 'Hello (1)',
+    russian: 'Здравствуйте',
+    hebrew: 'שלום',
+    pronuncEng: 'shalom',
+    pronuncRus: 'шалОм'
+  },
+
+  {
+    id: 2,
+    english: 'Good morning (2)',
+    russian: 'Доброе утро',
+    hebrew: 'בוקר טוב',
+    pronuncEng: 'boker tov',
+    pronuncRus: 'бОкер тов'
+  },
+
+  {
+    id: 3,
+    english: 'City (3)',
+    russian: 'Город',
+    hebrew: 'עיר',
+    pronuncEng: 'ir',
+    pronuncRus: 'ир'
+  },
+
+  {
+    id: 4,
+    english: 'Street (4)',
+    russian: 'Улица',
+    hebrew: 'רחוב',
+    pronuncEng: 'rekhOv',
+    pronuncRus: 'рэхОв'
+  },
+
+  {
+    id: 5,
+    english: 'Sun (5)',
+    russian: 'Солнце',
+    hebrew: 'שמש',
+    pronuncEng: 'shEmesh',
+    pronuncRus: 'шЭмеш'
+  },
+
+  {
+    id: 6,
+    english: 'Tree (6)',
+    russian: 'Дерево',
+    hebrew: 'עץ',
+    pronuncEng: 'ets',
+    pronuncRus: 'эц'
+  },
+
+  {
+    id: 7,
+    english: 'Air (7)',
+    russian: 'Воздух',
+    hebrew: 'אוויר',
+    pronuncEng: 'avir',
+    pronuncRus: 'авИр'
+  },
+
+  {
+    id: 8,
+    english: 'Red (8)',
+    russian: 'Красный',
+    hebrew: 'אדום',
+    pronuncEng: 'adOm',
+    pronuncRus: 'адОм'
+  },
+
+  {
+    id: 9,
+    english: 'Sea (9)',
+    russian: 'Море',
+    hebrew: 'ים',
+    pronuncEng: 'yam',
+    pronuncRus: 'ям'
+  },
+
+  {
+    id: 10,
+    english: 'Haifa (10)',
+    russian: 'Хайфа',
+    hebrew: 'חיפה',
+    pronuncEng: 'KheifA',
+    pronuncRus: 'ХэйфА'
+  },
+
+  {
+    id: 11,
+    english: 'To speak (infinitive) (11)',
+    russian: 'Разговаривать (инфинитив)',
+    hebrew: 'לדבר',
+    pronuncEng: 'ledabEr',
+    pronuncRus: 'ледабЭр'
+  },
+
+  {
+    id: 12,
+    english: 'see (present, single, m) (12)',
+    russian: 'Видеть (настоящее время, ед. число, муж. род)',
+    hebrew: 'רואה',
+    pronuncEng: 'roE',
+    pronuncRus: 'роЭ'
+  },
+
+  {
+    id: 13,
+    english: '',
+    russian: '',
+    hebrew: '',
+    pronuncEng: '',
+    pronuncRus: ''
+  }
+]
+
+let randomNumber = (Math.ceil(Math.random()*(words.length - 1)));
+
+
 
 function Milim() {
-  console.log(`**********************************`);
-  console.log(`Number of Already Used Words: ${alreadyUsedWords.length}`);
-  console.log(`Id's of already used words: ${alreadyUsedWords}`)
-
-  const words = [
-    {
-      id: 1,
-      english: 'Hello',
-      russian: 'Здравствуйте',
-      hebrew: 'שלום',
-      pronuncEng: 'shalom',
-      pronuncRus: 'шалОм'
-    },
-
-    {
-      id: 2,
-      english: 'Good morning',
-      russian: 'Доброе утро',
-      hebrew: 'בוקר טוב',
-      pronuncEng: 'boker tov',
-      pronuncRus: 'бОкер тов'
-    },
-
-    {
-      id: 3,
-      english: 'City',
-      russian: 'Город',
-      hebrew: 'עיר',
-      pronuncEng: 'ir',
-      pronuncRus: 'ир'
-    },
-
-    {
-      id: 4,
-      english: 'Street',
-      russian: 'Улица',
-      hebrew: 'רחוב',
-      pronuncEng: 'rekhOv',
-      pronuncRus: 'рэхОв'
-    },
-
-    {
-      id: 5,
-      english: 'Sun',
-      russian: 'Солнце',
-      hebrew: 'שמש',
-      pronuncEng: 'shEmesh',
-      pronuncRus: 'шЭмеш'
-    },
-
-    {
-      id: 6,
-      english: 'Tree',
-      russian: 'Дерево',
-      hebrew: 'עץ',
-      pronuncEng: 'ets',
-      pronuncRus: 'эц'
-    },
-
-    {
-      id: 7,
-      english: 'Air',
-      russian: 'Воздух',
-      hebrew: 'אוויר',
-      pronuncEng: 'avir',
-      pronuncRus: 'авИр'
-    },
-
-    {
-      id: 8,
-      english: 'Red',
-      russian: 'Красный',
-      hebrew: 'אדום',
-      pronuncEng: 'adOm',
-      pronuncRus: 'адОм'
-    },
-
-    {
-      id: 9,
-      english: 'Sea',
-      russian: 'Море',
-      hebrew: 'ים',
-      pronuncEng: 'yam',
-      pronuncRus: 'ям'
-    },
-
-    {
-      id: 10,
-      english: 'Haifa',
-      russian: 'Хайфа',
-      hebrew: 'חיפה',
-      pronuncEng: 'KheifA',
-      pronuncRus: 'ХэйфА'
-    },
-
-    {
-      id: 11,
-      english: 'To speak (infinitive)',
-      russian: 'Разговаривать (инфинитив)',
-      hebrew: 'לדבר',
-      pronuncEng: 'ledabEr',
-      pronuncRus: 'ледабЭр'
-    },
-
-    {
-      id: 12,
-      english: 'see (present, single, m)',
-      russian: 'Видеть (настоящее время, ед. число, муж. род)',
-      hebrew: 'רואה',
-      pronuncEng: 'roE',
-      pronuncRus: 'роЭ'
-    }
-  ]
-
-  const randomNumber = (Math.ceil(Math.random()*words.length));
-  const adjustedRandomNumber = (alreadyUsedWords.includes(randomNumber) 
-    ? 'this word was already used' 
-    : 'this is a new word');
-  
-  console.log(`Random: ${randomNumber}`);
-  console.log(`Adjusted Random: ${adjustedRandomNumber}`);
 
   // const { selectedLanguage } = useLanguage();
   const [inputValue, setInputValue] = useState('');
   const [language, setLanguage] = useState('english');
   const [score, setScore] = useState(0);
-  const [word, setWord] = useState(randomNumber);
+  const [word, setWord] = useState(randomNumber - 1);
+
+  // ^^^^^^^^^^^^^^^
+  // IMPORTANT thing here that I didn't see for some time. 
+  // We set the State of 'word' to 'randomNumber - 1', because useState gets a position
+  // of the word in the array, and NOT IT's ID. That's why if the State is 6, it shows the 7th word.
+  // Because the word in array have internal invisible numbers 0, 1, 2 etc.
+
   const [counter, setCounter] = useState(0);
   const [message, setMessage] = useState('');
   const [showPronunciation, setShowPronunciation] = useState(false);
@@ -152,16 +161,25 @@ function Milim() {
       setLanguage('english')
   }
 
-  console.log(`Counter before click: ${counter}`)
-
   const checkAnswer = () => {
+
+    console.log(`**********************************`);
+    console.log(`Number of Already Used Words: ${alreadyUsedWords.length}`);
+    console.log(`Id's of already used words: ${alreadyUsedWords}`)
+    
+    console.log(`Random: ${randomNumber}`);
+    console.log(`Counter before click: ${counter}`);
+
+    // if (!alreadyUsedWords.includes(randomNumber)) {
+
+    randomNumber = (Math.ceil(Math.random()*(words.length - 1)));
 
       if (inputValue === '') {
         language === 'english' ? 
         setMessage(`Type the word inside the input please`) :
         setMessage(`Напишите слово в поле, пожалуйста`);
       
-      } else if (inputValue === words[word].hebrew && counter < words.length - 1) {
+      } else if (inputValue === words[word].hebrew && counter < words.length - 2) {
 
         setScore(score + 1);
 
@@ -171,19 +189,25 @@ function Milim() {
         setMessage(`Верно! '${words[word].russian}' это <span id='orange'>${words[word].hebrew}</span>.
          <span id='pronunciation'}>Показать произношение</span>`)
 
-        alreadyUsedWords.push(words[word].id)
+        alreadyUsedWords.push(words[word]);
+        const currentWord = alreadyUsedWords[alreadyUsedWords.length - 1];
         console.log(alreadyUsedWords);
-        setWord(randomNumber);
+        console.log(currentWord);
+        console.log(currentWord.id);
+        setWord(randomNumber - 1);
         setCounter(counter + 1);
 
-      } else if (counter < words.length - 1) {
+      } else if (counter < words.length - 2) {
 
         setMessage(`<span id='orange'>${inputValue}</span> was incorrect. 
         Correct answer: <span id='orange'>${words[word].hebrew}</span>. <br>Try next word.`);
         
-        alreadyUsedWords.push(words[word].id)
+        alreadyUsedWords.push(words[word]);
+        const currentWord = alreadyUsedWords[alreadyUsedWords.length - 1];
         console.log(alreadyUsedWords);
-        setWord(randomNumber);
+        console.log(currentWord);
+        console.log(currentWord.id);
+        setWord(randomNumber - 1);
         setCounter(counter + 1);
 
       } else if (inputValue === words[word].hebrew) {
@@ -200,15 +224,20 @@ function Milim() {
     setShowPronunciation(false);
 
     console.log(`COUNTER AFTER CLICK: ${counter}`)
-  }
-
   
-
+//   } else {
+//   randomNumber = (Math.ceil(Math.random()*words.length));
+//   }
+  }
+  
   const restart = () => {
     setFinalResults(false);
     setScore(0);
-    setWord(0);
+    setWord(randomNumber - 1);
     setMessage(``);
+    setCounter(0);
+    alreadyUsedWords = [];
+
   }
 
   const pronounce = () => {
@@ -272,8 +301,8 @@ function Milim() {
           <h2>
           {
           language === 'english' ?
-          `${score} out of ${words.length} were answered correctly (${Math.round(score/words.length*10000)/100}%)` :
-          `${score} из ${words.length} слов были названы правильно (${Math.round(score/words.length*10000)/100}%)`
+          `${score} out of ${words.length - 1} were answered correctly (${Math.round(score/(words.length - 1)*10000)/100}%)` :
+          `${score} из ${words.length - 1} слов были названы правильно (${Math.round(score/(words.length - 1)*10000)/100}%)`
           }</h2>
         </div>
 
@@ -338,8 +367,8 @@ function Milim() {
         <h3 className='h3'>
         {
         language === 'english' ?
-        `Current score: ${score}/${words.length}` :
-        `Текущий счёт: ${score}/${words.length}`
+        `Current score: ${score}/${words.length - 1}` :
+        `Текущий счёт: ${score}/${words.length - 1}`
         }</h3>
         <div className='one-line-div'>
           <div className='h3' id='message' onClick={() => pronounce()}>
