d7112ab4b5a265212a943666efc8873aa1d92d5f
Grigorii Belov <belovgrigory139@gmail.com>
2023-10-20 23:54:35 +0300

    modified:   src/components/Chem.js 	new file:   src/data/chemQuestions.js

--- src/components/Chem.js
+++ src/components/Chem.js
@@ -1,15 +1,65 @@
 import React, { useState } from 'react';
 import './Chem.css';
+import chemQuestions from '../data/chemQuestions';
 
 function Chem() {
+    const [showFinalResults, setFinalResults] = useState(false);
     const [score, setScore] = useState(0);
     const [currentQuestion, setCurrentQuestion] = useState(0);
 
-    const questions = [
-        {
-            text: "",
+
+const optionClicked = (isCorrect) => {
+    if (isCorrect) {
+        setScore(score + 1);
+    }
+
+    if (currentQuestion + 1 < chemQuestions.length) {
+        setCurrentQuestion(currentQuestion + 1);
+    } else {
+        setFinalResults(true);
+    }
+}
+
+const restartGame = () => {
+    setScore(0);
+    setCurrentQuestion(0);
+    setFinalResults(false);
+}
+
+return (
+    <div className="Chem">
+        <h2>Typical Questions for IB</h2>
+
+        <h2>Current Score: {score}</h2>
+
+        { showFinalResults ? (
+            <div className='final-results'>
+                <h2>Final Results</h2>
+                <h2>{score} out of {chemQuestions.length} correct - ({(score/chemQuestions.length)*100}%)</h2>
+            <button className='restart-button' onClick={() => restartGame()}>Restart Game</button>
+            </div>
+        ) : (
+            <div className='question-card'>
+            <h2>Question {currentQuestion + 1} out of {chemQuestions.length}</h2>
+            <h3 className='question-text'>{questions[currentQuestion].text}</h3>
+
+        <ul>
+            {questions[currentQuestion].options.map((option) => {
+                return (
+                    <li onClick={() => optionClicked(option.isCorrect)} key={option.id}>{option.text}</li>
+                );
+            })}
+        </ul>
+
+        </div>
+
+        )
+
         }
-    ]
+
+    </div>
+);
+
 }
 
 export default Chem
---
+++ src/data/chemQuestions.js
@@ -0,0 +1,16 @@
+const chemQuestions = [
+    {
+        text: "Using a copy of the periodic table, what is the correct electronic configuration for an atom of Bromine, Br (Z = 35)?",
+        options: [
+            {id: 0, text: "[Ar] 5s²5p⁵"},
+            {id: 1, text: ""},
+            {id: 2, text: ""},
+            {id: 3, text: ""}
+        ],
+    },
+
+
+
+]
+
+export default chemQuestions
