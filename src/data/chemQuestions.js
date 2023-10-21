a7e6c78c325836ac4d01cf7b56f5d2d793f4a014
Grigorii Belov <belovgrigory139@gmail.com>
2023-10-21 23:31:22 +0300

    modified:   src/data/chemQuestions.js

--- src/data/chemQuestions.js
+++ src/data/chemQuestions.js
@@ -2,11 +2,45 @@ const chemQuestions = [
     {
         text: "Using a copy of the periodic table, what is the correct electronic configuration for an atom of Bromine, Br (Z = 35)?",
         options: [
-            {id: 0, text: "[Ar] 5s²5p⁵"},
-            {id: 1, text: ""},
-            {id: 2, text: ""},
-            {id: 3, text: ""}
+            {id: 0, text: "[Ar] 5s²5p⁵", isCorrect: false},
+            {id: 1, text: "1s²2s²2p⁶3s²3p⁶4s²3d¹⁰4p⁵", isCorrect: true},
+            {id: 2, text: "[Ar] 4s²4p⁵", isCorrect: false},
+            {id: 3, text: "1s²2s²2p⁶3s²3p⁶4s²3d¹⁰4p⁶", isCorrect: true}
         ],
+        category: "Periodic table"
+    },
+
+    {
+        text: "Using a copy of the periodic table, what is the correct electronic configuration for an atom of Bismuth, Bi (Z = 83)?",
+        options: [
+            {id: 0, text: "[Xe] 6s²6p⁵", isCorrect: false},
+            {id: 1, text: "1s²2s²2p⁶3s²3p⁶4s²3d¹⁰4p⁶5s²4d¹⁰5p³", isCorrect: false},
+            {id: 2, text: "[Xe] 6s²4f¹⁴5d¹⁰6p³", isCorrect: true},
+            {id: 3, text: "1s²2s²2p⁶3s²3p⁶4s²3d¹⁰4p⁶5s²4d¹⁰5p⁶6s²4f¹⁴5d¹⁰6p⁵", isCorrect: false}
+        ],
+        category: "Periodic table"
+    },
+
+    {
+        text: "What name do we give to group 17?",
+        options: [
+            {id: 0, text: "Saltmakers", isCorrect: false},
+            {id: 1, text: "Noble gases", isCorrect: false},
+            {id: 2, text: "Chalcogens", isCorrect: false},
+            {id: 3, text: "Halogens", isCorrect: true}
+        ],
+        category: "Periodic table"
+    },
+
+    {
+        text: "",
+        options: [
+            {id: 0, text: "", isCorrect: false},
+            {id: 1, text: "", isCorrect: false},
+            {id: 2, text: "", isCorrect: false},
+            {id: 3, text: "", isCorrect: false}
+        ],
+        category: "Periodic table"
     },
 
 
