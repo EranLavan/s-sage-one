4759dba3f09d41f70ce68ccc4a17620ca7ef993d
Grigorii Belov <belovgrigory139@gmail.com>
2024-01-23 00:15:56 +0300

    modified:   src/data/itWords.js

--- src/data/itWords.js
+++ src/data/itWords.js
@@ -11,181 +11,181 @@ const itWords = [
 
   {
     id: 2,
-    english: '',
+    english: 'Line length',
     russian: 'длина записи',
-    hebrew: '',
-    pronuncEng: '',
-    pronuncRus: '',
+    hebrew: 'אורך רשומה',
+    pronuncEng: 'Orekh reshumA',
+    pronuncRus: 'Орэх решумА',
     category: 'it'
   },
 
   {
     id: 3,
-    english: '',
+    english: 'Storage',
     russian: 'хранение',
-    hebrew: '',
-    pronuncEng: '',
-    pronuncRus: '',
+    hebrew: 'אחסון',
+    pronuncEng: 'ikhsUn',
+    pronuncRus: 'ихсУн',
     category: 'it'
   },
 
   {
     id: 4,
-    english: '',
+    english: 'Localisation',
     russian: 'локализация',
-    hebrew: '',
-    pronuncEng: '',
-    pronuncRus: '',
+    hebrew: 'איתור',
+    pronuncEng: 'itUr',
+    pronuncRus: 'итУр',
     category: 'it'
   },
 
   {
     id: 5,
-    english: '',
+    english: 'Event',
     russian: 'событие',
-    hebrew: '',
-    pronuncEng: '',
-    pronuncRus: '',
+    hebrew: 'ארוע',
+    pronuncEng: 'erUa',
+    pronuncRus: 'эрУа',
     category: 'it'
   },
 
   {
     id: 6,
-    english: '',
-    russian: '',
-    hebrew: '',
-    pronuncEng: '',
-    pronuncRus: '',
+    english: 'Website',
+    russian: 'сайт',
+    hebrew: 'אתר',
+    pronuncEng: 'atAr',
+    pronuncRus: 'атАр',
     category: 'it'
   },
 
   {
     id: 7,
-    english: '',
-    russian: '',
-    hebrew: '',
-    pronuncEng: '',
-    pronuncRus: '',
+    english: 'App testing',
+    russian: 'тестирование приложения',
+    hebrew: 'בדיקת תוכנה',
+    pronuncEng: 'bdikAt tokhnA',
+    pronuncRus: 'бдикАт тохнА',
     category: 'it'
   },
 
   {
     id: 8,
-    english: '',
-    russian: '',
-    hebrew: '',
-    pronuncEng: '',
-    pronuncRus: '',
+    english: 'QA',
+    russian: 'QA',
+    hebrew: 'בקרת איכות',
+    pronuncEng: 'bakarAt eikhUt',
+    pronuncRus: 'бакарАт эйхУт',
     category: 'it'
   },
 
   {
     id: 9,
-    english: '',
-    russian: '',
-    hebrew: '',
-    pronuncEng: '',
-    pronuncRus: '',
+    english: 'Default',
+    russian: 'дефолт',
+    hebrew: 'ברירת מחדל',
+    pronuncEng: 'brerAt mekhdAl',
+    pronuncRus: 'брерАт мехдАл',
     category: 'it'
   },
 
   {
     id: 10,
-    english: '',
-    russian: '',
-    hebrew: '',
-    pronuncEng: '',
-    pronuncRus: '',
+    english: 'microchip, IC (integrated circuit)',
+    russian: 'чип',
+    hebrew: "ג'וק / שבב",
+    pronuncEng: 'juk / shvav',
+    pronuncRus: 'джУк / швАв',
     category: 'it'
   },
 
   {
     id: 11,
-    english: '',
-    russian: '',
-    hebrew: '',
-    pronuncEng: '',
-    pronuncRus: '',
+    english: 'Font',
+    russian: 'шрифт',
+    hebrew: 'גופן',
+    pronuncEng: 'gofAn',
+    pronuncRus: 'гофАн',
     category: 'it'
   },
 
   {
     id: 12,
-    english: '',
-    russian: '',
-    hebrew: '',
-    pronuncEng: '',
-    pronuncRus: '',
+    english: 'Cut',
+    russian: 'вырезать',
+    hebrew: 'גזור',
+    pronuncEng: 'gzor',
+    pronuncRus: 'гзор',
     category: 'it'
   },
 
   {
     id: 13,
-    english: '',
-    russian: '',
-    hebrew: '',
-    pronuncEng: '',
-    pronuncRus: '',
+    english: 'Backup',
+    russian: 'бэкап',
+    hebrew: 'גיבוי',
+    pronuncEng: 'gibUi',
+    pronuncRus: 'гибУй',
     category: 'it'
   },
 
   {
     id: 14,
-    english: '',
-    russian: '',
-    hebrew: '',
-    pronuncEng: '',
-    pronuncRus: '',
+    english: 'Version',
+    russian: 'версия',
+    hebrew: 'גירסה',
+    pronuncEng: 'girsA',
+    pronuncRus: 'гирсА',
     category: 'it'
   },
 
   {
     id: 15,
-    english: '',
-    russian: '',
-    hebrew: '',
-    pronuncEng: '',
-    pronuncRus: '',
+    english: 'Web surfing',
+    russian: 'серфинг в интернете',
+    hebrew: 'גלישה באינטרנט',
+    pronuncEng: 'glishA internet',
+    pronuncRus: 'глишА Интернет',
     category: 'it'
   },
 
   {
     id: 16,
-    english: '',
-    russian: '',
-    hebrew: '',
-    pronuncEng: '',
-    pronuncRus: '',
+    english: 'Flag',
+    russian: 'флаг',
+    hebrew: 'דגל',
+    pronuncEng: 'dEgel',
+    pronuncRus: 'дЭгэль',
     category: 'it'
   },
 
   {
     id: 17,
-    english: '',
-    russian: '',
-    hebrew: '',
-    pronuncEng: '',
-    pronuncRus: '',
+    english: 'Model',
+    russian: 'модель',
+    hebrew: 'דגם',
+    pronuncEng: 'dEgem',
+    pronuncRus: 'дЭгэм',
     category: 'it'
   },
 
   {
     id: 18,
-    english: '',
-    russian: '',
-    hebrew: '',
-    pronuncEng: '',
-    pronuncRus: '',
+    english: 'Email',
+    russian: 'имэйл',
+    hebrew: 'דואר אלקטרוני',
+    pronuncEng: 'dOar elektroni',
+    pronuncRus: 'дОар электрОни',
     category: 'it'
   },
 
   {
     id: 19,
-    english: '',
-    russian: '',
-    hebrew: '',
-    pronuncEng: '',
-    pronuncRus: '',
+    english: 'Compression',
+    russian: 'сжатие',
+    hebrew: 'דחיסה',
+    pronuncEng: 'dkhisA',
+    pronuncRus: 'дхисА',
     category: 'it'
   },
 
