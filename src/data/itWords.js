4c8f871be6a70aed01e5e66f1b6a7945f2dcd391
Grigorii Belov <belovgrigory139@gmail.com>
2024-01-22 23:58:45 +0300

    new file:   src/data/itWords.js

---
+++ src/data/itWords.js
@@ -0,0 +1,203 @@
+const itWords = [
+  {
+    id: 1,
+    english: 'Security',
+    russian: 'Защита, секьюрность',
+    hebrew: 'אבטחה',
+    pronuncEng: 'avtakhA',
+    pronuncRus: 'автахА',
+    category: 'it'
+  },
+
+  {
+    id: 2,
+    english: '',
+    russian: 'длина записи',
+    hebrew: '',
+    pronuncEng: '',
+    pronuncRus: '',
+    category: 'it'
+  },
+
+  {
+    id: 3,
+    english: '',
+    russian: 'хранение',
+    hebrew: '',
+    pronuncEng: '',
+    pronuncRus: '',
+    category: 'it'
+  },
+
+  {
+    id: 4,
+    english: '',
+    russian: 'локализация',
+    hebrew: '',
+    pronuncEng: '',
+    pronuncRus: '',
+    category: 'it'
+  },
+
+  {
+    id: 5,
+    english: '',
+    russian: 'событие',
+    hebrew: '',
+    pronuncEng: '',
+    pronuncRus: '',
+    category: 'it'
+  },
+
+  {
+    id: 6,
+    english: '',
+    russian: '',
+    hebrew: '',
+    pronuncEng: '',
+    pronuncRus: '',
+    category: 'it'
+  },
+
+  {
+    id: 7,
+    english: '',
+    russian: '',
+    hebrew: '',
+    pronuncEng: '',
+    pronuncRus: '',
+    category: 'it'
+  },
+
+  {
+    id: 8,
+    english: '',
+    russian: '',
+    hebrew: '',
+    pronuncEng: '',
+    pronuncRus: '',
+    category: 'it'
+  },
+
+  {
+    id: 9,
+    english: '',
+    russian: '',
+    hebrew: '',
+    pronuncEng: '',
+    pronuncRus: '',
+    category: 'it'
+  },
+
+  {
+    id: 10,
+    english: '',
+    russian: '',
+    hebrew: '',
+    pronuncEng: '',
+    pronuncRus: '',
+    category: 'it'
+  },
+
+  {
+    id: 11,
+    english: '',
+    russian: '',
+    hebrew: '',
+    pronuncEng: '',
+    pronuncRus: '',
+    category: 'it'
+  },
+
+  {
+    id: 12,
+    english: '',
+    russian: '',
+    hebrew: '',
+    pronuncEng: '',
+    pronuncRus: '',
+    category: 'it'
+  },
+
+  {
+    id: 13,
+    english: '',
+    russian: '',
+    hebrew: '',
+    pronuncEng: '',
+    pronuncRus: '',
+    category: 'it'
+  },
+
+  {
+    id: 14,
+    english: '',
+    russian: '',
+    hebrew: '',
+    pronuncEng: '',
+    pronuncRus: '',
+    category: 'it'
+  },
+
+  {
+    id: 15,
+    english: '',
+    russian: '',
+    hebrew: '',
+    pronuncEng: '',
+    pronuncRus: '',
+    category: 'it'
+  },
+
+  {
+    id: 16,
+    english: '',
+    russian: '',
+    hebrew: '',
+    pronuncEng: '',
+    pronuncRus: '',
+    category: 'it'
+  },
+
+  {
+    id: 17,
+    english: '',
+    russian: '',
+    hebrew: '',
+    pronuncEng: '',
+    pronuncRus: '',
+    category: 'it'
+  },
+
+  {
+    id: 18,
+    english: '',
+    russian: '',
+    hebrew: '',
+    pronuncEng: '',
+    pronuncRus: '',
+    category: 'it'
+  },
+
+  {
+    id: 19,
+    english: '',
+    russian: '',
+    hebrew: '',
+    pronuncEng: '',
+    pronuncRus: '',
+    category: 'it'
+  },
+
+  {
+    id: 20,
+    english: '',
+    russian: '',
+    hebrew: '',
+    pronuncEng: '',
+    pronuncRus: '',
+    category: 'it'
+  },
+
+
+]
